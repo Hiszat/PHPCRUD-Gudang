8150f7615cd3fe91964cc0401f84a70af16b7c51
Izat <hiszattamvan21@gmail.com>
2023-11-18 21:13:34 +0700

    Changing small things

--- simpanbarang.php
+++ simpanbarang.php
@@ -1,6 +1,7 @@
 <?php
     include('koneksi.php');
-    $sql = "INSERT INTO barang (kode_barang, nama_barang, jumlah, harga_beli, harga_jual) VALUES ('$_POST[kode_barang]', '$_POST[nama_barang]', '$_POST[jumlah]', '$_POST[harga_beli]', '$_POST[harga_jual]')";
+    $sql = "INSERT INTO barang (kode_barang, nama_barang, jumlah, harga_beli, harga_jual) 
+    VALUES ('$_POST[kode_barang]', '$_POST[nama_barang]', '$_POST[jumlah]', '$_POST[harga_beli]', '$_POST[harga_jual]')";
 
     $query = mysqli_query($konek, $sql);
     if($query) {    
